c6aa1a1c898ae85f1475f3305d4bd1b1fc1409c4
Oktay Menteş <oktay.ment02@gmail.com>
2018-06-08 05:39:49 +0300

    Create README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+rehberKayitUygulamasi
+
+
+mvc ile telefon kaydetmek için yaplımış küçük bir rehber uygulaması
---
+++ rehberKayitUygulamasi/Controllers/RehberController.cs
@@ -0,0 +1,135 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using rehberKayitUygulamasi.Models;
+namespace rehberKayitUygulamasi.Controllers
+{
+    public class RehberController : Controller
+    {
+        
+
+        dbRehberimEntities db = new dbRehberimEntities();
+
+        // var olan bütün kayıtları gösterir
+        public ActionResult Index()
+        {
+           var kayitlistesi= db.tblKayitlars.ToList();
+
+            return View(kayitlistesi);
+        }
+
+        [HttpGet]
+        //yeni kayıt isteği atılınca çalışır
+        public ActionResult kayitFormu()
+        {
+            
+            
+            return View(new tblKayitlar());
+        }
+        
+
+        [HttpPost]
+        //post isteği atılırsa çalışır
+        public ActionResult kayitFormu(tblKayitlar kayit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(kayit);
+            }
+            if (kayit.Id==0)
+            {
+                
+                var kayitZatenVar = from b in db.tblKayitlars
+                            where (b.tel==kayit.tel)
+                            select b;
+                if (kayitZatenVar.Count()==0)
+                {
+                    
+                    db.tblKayitlars.Add(kayit);
+                }
+                else
+                {
+
+                    ViewBag.HtmlStr = "Telefon numarası zaten kayıtlı!";
+                    return View("kayitFormu", kayit);
+                }
+                
+            }
+            else
+            {
+                //numara zaten kayıtlı mı değil mi kontrol için 
+                var kayitZatenVar = from b in db.tblKayitlars
+                                    where (b.tel == kayit.tel)
+                                    select b;
+                var guncellenecekKayit = db.tblKayitlars.Find(kayit.Id);
+
+                // hiç değişiklik yapmadan kaydedilirse çalışır, sql e ikide bir istek atmaya gerek yok :) 
+                if (guncellenecekKayit==kayitZatenVar.First())
+                {
+                    
+                    return RedirectToAction("Index");
+                }
+                //aynı kayıt güncellenecekse çalışır
+                if (kayitZatenVar.Count() != 0 && kayitZatenVar.First().Id!=guncellenecekKayit.Id)
+                {
+
+                    ViewBag.HtmlStr = "Bu telefon numarası "+kayitZatenVar.First().ad +" adı ile zaten kayıtlı!";
+                    return View("kayitFormu", kayit);
+                }
+                //url den id si olmayan bir model atılırsa çalışır
+                else if (guncellenecekKayit==null)
+                {
+                    return HttpNotFound();
+                }
+                guncellenecekKayit.ad = kayit.ad;
+                guncellenecekKayit.soyAd = kayit.soyAd;
+                guncellenecekKayit.tel = kayit.tel;
+            }
+            
+            db.SaveChanges();
+
+            return RedirectToAction("Index","Rehber");
+        }
+        //formda düzenle butonuna basılınca bu method çağırılır, client tarafında halletmeye çalışacağım olursa
+        public ActionResult guncelle(int id)
+        {
+            var kayit = db.tblKayitlars.Find(id);
+            if (kayit==null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("kayitFormu",kayit);
+        }
+
+        //kayıt silme methodu
+        public ActionResult sil(int id)
+        {
+            
+            db.tblKayitlars.Remove(db.tblKayitlars.Find(id));
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Rehber");
+        }
+
+        //sql de numaraları düzenleyemedim, ama c# ile düzenledim :)
+        public ActionResult numaraDuzenleyici()
+        {
+
+            var liste = db.tblKayitlars.ToList();
+            foreach (var item in liste)
+            {
+                var guncellenecekKayit = db.tblKayitlars.Find(item.Id);
+                guncellenecekKayit.tel=guncellenecekKayit.tel.Replace(".", "");
+                guncellenecekKayit.tel = guncellenecekKayit.tel.Replace(")", "");
+                guncellenecekKayit.tel = guncellenecekKayit.tel.Replace("(", "");
+                guncellenecekKayit.tel = guncellenecekKayit.tel.Replace(" ", "");
+                guncellenecekKayit.tel = guncellenecekKayit.tel.Replace("-", "");
+                
+                db.SaveChanges();
+            }
+            
+
+            return View();
+        }
+    }
+}
